722d80f28134eb357605bceed9fbecef6017484d
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2018-11-13 11:35:21 +0100

    Production All Projects as One

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use("/images", express.static(path.join("images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
 //Add CORS
 app.use((req, res, next) => {
@@ -45,5 +46,8 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/auth", authRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
